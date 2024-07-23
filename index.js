5c458575f02d092b77f312d0ed45b432b23e1065
Ankit yadav <raoankit0022@gmail.com>
2024-07-23 22:24:43 +0530

    backend initailse

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require ("express");
+
+const urlRoute = require ("./routes/url");
+const { connectDB } = require("./db");
+const URL= require ('./models/url')
+const app = express();
+const PORT = 4000;
+connectDB("mongodb+srv://raoankit0022:Z2XP9lF8ECS4AnXk@cluster0.ypohhif.mongodb.net/shortURL")
+.then(
+    ()=>console.log('connected to mongodb successfully')
+)
+app.use (express.json());
+app.use("/url",urlRoute);
+app.get('/:shortId',async(req,res)=>{
+const shortId =req.params.shortId;
+const entry = await URL.findOneAndUpdate({
+shortId
+},{$push:{
+visitHistory:{
+    timestamp:Date.now(),
+}
+}
+}
+);
+res.redirect(entry.redirectURL)
+});
+
+app.listen(PORT,()=>console.log(`server is started perfectly:${PORT} `))
